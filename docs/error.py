5530ea9337e6945e0d5239df458e4208d860ce40
h0513 <handle@null.net>
2025-03-28 19:52:29 +0800

    Update error.py

--- docs/error.py
+++ docs/error.py
@@ -1,1 +1,9 @@
-g1
+g-1 connection to drone failed [func: ckcon()]
+b-2 drone battery level less than 10 [func: pref()]
+b-3 drone battery level less than 20 [func: pref()]
+pf-4 drone startup failed [func: start()]
+g-5 connection to drone userapi failed [func: connect()]
+g-6 pckcon not defined [func: ckcon()]
+c-7 qrcode alignment failed [func: qra()]
+c-8 qrcode recognition failed [func: qrr()]
+c-9 qrcode tracking failed [func: qrt()]
