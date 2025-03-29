bdbbe148f730460b58178262dc28d43dcc3c24f6
h0513 <handle@null.net>
2025-03-29 09:40:51 +0800

    Update s1.py
    fixed indentation

--- scripts/s1.py
+++ scripts/s1.py
@@ -1,15 +1,15 @@
-    import module
-    import drone as d
-    import output as o
-    import time
-    
-    d.start()
-    d.vt()
-    d.vu(50, True)
-    for i in range(3):
-        d.mf(70)
-        d.qrc(0, 4)
-        o.out(f"h {i+1}: {d.dh()}")
-        time.sleep(1)
-    d.vl()
-     
+import module
+import drone as d
+import output as o
+import time
+
+d.start()
+d.vt()
+d.vu(50, True)
+for i in range(3):
+    d.mf(70)
+    d.qrc(0, 4)
+    o.out(f"h {i+1}: {d.dh()}")
+    time.sleep(1)
+d.vl()
+ 
