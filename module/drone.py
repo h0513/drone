836eb7bf1e9c0cfa908604183adeb76d182704f7
h0513 <handle@null.net>
2025-04-05 19:41:26 +0800

    Update drone.py

--- module/drone.py
+++ module/drone.py
@@ -6,8 +6,7 @@ import keyboard
 from typing import Dict, Union, List
 import numpy as np
 import cv2
- 
- 
+
 led: Dict[str, Dict[str, int]] = {
     "n": {'g': 0, 'b': 0, 'r': 0, 'mode': 1}, # null
     "g": {'g': 0, 'b': 0, 'r': 255, 'mode': 1}, # green
@@ -19,29 +18,27 @@ led: Dict[str, Dict[str, int]] = {
     "w": {'g': 255, 'b': 255, 'r': 255, 'mode': 1} # white
 }
 PPREF: bool = False
- 
+
 def start(vb: bool=True):
-    print()
     out.inf("starting new drone session")
-    if vb: out.inf("starting drone")
     suc = True
     if not connect(vb): suc = False
     if not pref(True, vb): suc = False
+    keyboard.add_hotkey("ctrl+space", lambda: vl())
     if vb and suc: out.suc("completed drone startup")
     if vb and not suc: out.err(3)
-    keyboard.add_hotkey("ctrl+space", lambda: vl())
     return 0
- 
-def connect(vb: bool=True):
+
+def connect(vb: bool=False):
     if vb: out.inf("initiating connection to drone")
     global api
     api = hula.UserApi()
     ckcon(True, True)
     if ckcon():
-        if vb: out.suc("completed drone connection")
+        if vb: out.suc("connected to drone")
         return 1
     else: return 0
- 
+
 def ckcon(ins: bool=False, vb: bool=False):
     if not "api" in globals():
         if vb: out.err(5)
@@ -62,8 +59,8 @@ def ckcon(ins: bool=False, vb: bool=False):
         if vb: out.suc("drone successfully connected without pcon")
         pcon = True
         return 1
- 
-def pref(ins=True, vb=True):
+
+def preflight(ins=True, vb=True):
     if not ckcon():
         return 0
     if vb: out.inf("conducting preflight check")
@@ -80,181 +77,155 @@ def pref(ins=True, vb=True):
         PPREF = True
         if suc: return 1
         else: return 0
- 
+
 def mf(ds=0, vb=False, led=led["n"]):
-    if ckcon():
-        if vb: out.inf(f"moving {ds}cm forwards with led {led}")
-        api.single_fly_forward(ds, led)
-        if vb: out.suc(f"completed {ds}cm forwards motion with led {led}")
- 
+    if vb: out.inf(f"moving {ds}cm forwards with led {led}")
+    api.single_fly_forward(ds, led)
+    if vb: out.suc(f"completed {ds}cm forwards motion with led {led}")
+
 def mb(ds=0, vb=False, led=led["n"]):
-    if ckcon():
-        if vb: out.inf(f"moving {ds}cm backwards with led {led}")
-        api.single_fly_back(ds, led)
-        if vb: out.suc(f"completed {ds}cm backwards motion with led {led}")
- 
+    if vb: out.inf(f"moving {ds}cm backwards with led {led}")
+    api.single_fly_back(ds, led)
+    if vb: out.suc(f"completed {ds}cm backwards motion with led {led}")
+
 def ml(ds=0, vb=False, led=led["n"]):
-    if ckcon():
-        if vb: out.inf(f"moving {ds}cm left with led {led}")
-        api.single_fly_left(ds, led)
-        if vb: out.suc(f"completed {ds}cm left motion with led {led}")
- 
+    if vb: out.inf(f"moving {ds}cm left with led {led}")
+    api.single_fly_left(ds, led)
+    if vb: out.suc(f"completed {ds}cm left motion with led {led}")
+
 def mr(ds=0, vb=False, led=led["n"]):
-    if ckcon():
-        if vb: out.inf(f"moving {ds}cm right with led {led}")
-        api.single_fly_right(ds, led)
-        if vb: out.suc(f"completed {ds}cm right motion with led {led}")
- 
-def vt(vb=False, led=led["y"]):
-    if ckcon():
-        if vb: out.inf(f"taking off with led {led}")
-        api.single_fly_takeoff(led)
-        if vb: out.suc(f"completed takeoff with led {led}")
- 
-def vl(vb=False, led=led["y"]):
-    if ckcon():
-        if vb: out.inf(f"landing with led {led}")
-        api.single_fly_touchdown(led)
-        if vb: out.suc(f"completed landing with led {led}")
- 
-def vh(t=0, vb=False, led=led["n"]):
-    if ckcon():
-        if vb: out.inf(f"hovering for {t}s with led {led}")
-        api.single_fly_hover_flight(t, led)
-        if vb: out.suc(f"hovered for {t}s with led {led}")
- 
-def vu(ds=0, vb=False, led=led["n"]):
-    if ckcon():
-        if vb: out.inf(f"moving {ds}cm up with led {led}")
-        api.single_fly_up(ds, led)
-        if vb: out.suc(f"completed {ds}cm up motion with led {led}")
- 
-def vd(ds=0, vb=False, led=led["n"]):
-    if ckcon():
-        if vb: out.inf(f"moving {ds}cm down with led {led}")
-        api.single_fly_down(ds, led)
-        if vb: out.suc(f"completed {ds}cm down motion with led {led}")
- 
+    if vb: out.inf(f"moving {ds}cm right with led {led}")
+    api.single_fly_right(ds, led)
+    if vb: out.suc(f"completed {ds}cm right motion with led {led}")
+
+def takeoff(vb=False, led=led["y"]):
+    if vb: out.inf(f"taking off with led {led}")
+    api.single_fly_takeoff(led)
+    if vb: out.suc(f"completed takeoff with led {led}")
+
+def land(vb=False, led=led["y"]):
+    if vb: out.inf(f"landing with led {led}")
+    api.single_fly_touchdown(led)
+    if vb: out.suc(f"completed landing with led {led}")
+
+def hover(t=0, vb=False, led=led["n"]):
+    if vb: out.inf(f"hovering for {t}s with led {led}")
+    api.single_fly_hover_flight(t, led)
+    if vb: out.suc(f"hovered for {t}s with led {led}")
+
+def ascend(ds=0, vb=False, led=led["n"]):
+    if vb: out.inf(f"moving {ds}cm up with led {led}")
+    api.single_fly_up(ds, led)
+    if vb: out.suc(f"completed {ds}cm up motion with led {led}")
+
+def descend(ds=0, vb=False, led=led["n"]):
+    if vb: out.inf(f"moving {ds}cm down with led {led}")
+    api.single_fly_down(ds, led)
+    if vb: out.suc(f"completed {ds}cm down motion with led {led}")
+
 def rl(an=0, vb=False, led=led["n"]):
-    if ckcon():
-        if vb: out.inf(f"turning {an} degree left with led {led}")
-        api.single_fly_turnleft(an, led)
-        if vb: out.suc(f"completed {an} degree left turn with led {led}")
- 
+    if vb: out.inf(f"turning {an} degree left with led {led}")
+    api.single_fly_turnleft(an, led)
+    if vb: out.suc(f"completed {an} degree left turn with led {led}")
+
 def rr(an=0, vb=False, led=led["n"]):
-    if ckcon():
-        if vb: out.inf(f"turning {an} degree right with led {led}")
-        api.single_fly_turnright(an, led)
-        if vb: out.suc(f"completed {an} degree right turn with led {led}")
- 
+    if vb: out.inf(f"turning {an} degree right with led {led}")
+    api.single_fly_turnright(an, led)
+    if vb: out.suc(f"completed {an} degree right turn with led {led}")
+
 def cf(r=0, vb=False, led=led["n"]):
-    if ckcon():
-        if vb: out.inf(f"flying {r}cm r circular path with led {led}")
-        api.single_fly_radius_around(r, led)
-        if vb: out.suc(f"completed {r}cm r circular path with led {led}")
- 
-def dh(vb=False):
-    if ckcon():
-        if vb: out.inf("getting drone tof height")
-        h = api.get_plane_ds()
-        if vb: out.suc(f"completed drone tof height detection [output: {h}]")
-        return h
- 
+    if vb: out.inf(f"flying {r}cm r circular path with led {led}")
+    api.single_fly_radius_around(r, led)
+    if vb: out.suc(f"completed {r}cm r circular path with led {led}")
+
+def height(vb=False):
+    if vb: out.inf("getting drone tof height")
+    h = api.get_plane_ds()
+    if vb: out.suc(f"completed drone tof height detection [output: {h}]")
+    return h
+
 def qra(id=0, md=0, vb=False):
-    if ckcon():
-        if vb: out.inf(f"aligning to qr {id} using mode {md}")
-        suc = api.single_fly_Qrcode_align(md, id)
-        if suc: out.suc(f"completed alignment to qr {id} using mode {md}")
-        else: out.err(7)
- 
+    if vb: out.inf(f"aligning to qr {id} using mode {md}")
+    suc = api.single_fly_Qrcode_align(md, id)
+    if suc: out.suc(f"completed alignment to qr {id} using mode {md}")
+    else: out.err(7)
+
 def qrr(id=0, md=0, vb=False):
-    if ckcon():
-        if vb: out.inf(f"recognising qr {id} using mode {md}")
-        suc = api.single_fly_recognition_Qrcode(md, id)
-        if suc[0]: out.suc(f"completed recognition of qr {id} using mode {md}")
-        else: out.err(8)
-        return suc
- 
+    if vb: out.inf(f"recognising qr {id} using mode {md}")
+    suc = api.single_fly_recognition_Qrcode(md, id)
+    if suc[0]: out.suc(f"completed recognition of qr {id} using mode {md}")
+    else: out.err(8)
+    return suc
+
 def qrt(t=0, id=0, md=0, vb=False):
-    if ckcon():
-        if vb: out.inf(f"tracking qr {id} for {t}s")
-        suc = api.single_fly_track_Qrcode(id, t)
-        if suc: out.suc(f"completed tracking of qr {id} for {t}s")
-        else: out.err(9)
- 
-def ll(pset="-1", t=0, r=0, g=0, b=0, md=1, vb=False):
-    if ckcon():
-        if not pset == "-1":
-            if vb: out.inf(f"setting led color to {led[pset]} for {t}s")
-            suc = api.single_fly_lamplight(led[pset]["r"], led[pset]["g"], led[pset]["b"], t, md)
-            if suc and vb: out.suc(f"completed setting led color to {led[pset]} for {t}s")
-            else: out.err(10)
-        else:
-            if vb: out.inf(f"setting led color to r: {r}, g: {g}, b: {b} for {t}s")
-            suc = api.single_fly_lamplight(r, g, b, t, md)
-            if suc and vb: out.suc(f"completed setting led color to r: {r}, g: {g}, b: {b} for {t}s")
-            else: out.err(10)
- 
+    if vb: out.inf(f"tracking qr {id} for {t}s")
+    suc = api.single_fly_track_Qrcode(id, t)
+    if suc: out.suc(f"completed tracking of qr {id} for {t}s")
+    else: out.err(9)
+
+def light(pset="-1", t=0, r=0, g=0, b=0, md=1, vb=False):
+    if not pset == "-1":
+        if vb: out.inf(f"setting led color to {led[pset]} for {t}s")
+        suc = api.single_fly_lamplight(led[pset]["r"], led[pset]["g"], led[pset]["b"], t, md)
+        if suc and vb: out.suc(f"completed setting led color to {led[pset]} for {t}s")
+        else: out.err(10)
+    else:
+        if vb: out.inf(f"setting led color to r: {r}, g: {g}, b: {b} for {t}s")
+        suc = api.single_fly_lamplight(r, g, b, t, md)
+        if suc and vb: out.suc(f"completed setting led color to r: {r}, g: {g}, b: {b} for {t}s")
+        else: out.err(10)
+
 def fly(func, sf=True, vb=False):
     if sf: func = "single_fly_" + func
     func = "api." + func
-    if ckcon():
-        if vb: out.inf("executing", func)
-        exec(func)
-        if vb: out.inf("completed", func)
- 
+    if vb: out.inf("executing", func)
+    exec(func)
+    if vb: out.inf("completed", func)
+
 def vinit(vb=False):
-    if ckcon():
-        if vb: out.inf("initialising video")
-        global v
-        v = vid.hula_video(hula_api=api)
-        if vb: out.suc("completed video initalisation")
- 
-def vc(vb=False):
-    if ckcon():
-        if vb: out.inf("closing video stream")
-        v.close()
-        if vb: out.suc("completed video stream closing")
- 
+    if vb: out.inf("initialising video")
+    global v
+    v = vid.hula_video(hula_api=api)
+    if vb: out.suc("completed video initalisation")
+
+def vclose(vb=False):
+    if vb: out.inf("closing video stream")
+    v.close()
+    if vb: out.suc("completed video stream closing")
+
 def von(vb=False):
-    if ckcon():
-        if vb: out.inf("starting video stream")
-        v.video_mode_on()
-        if vb: out.suc("completed video")
- 
+    if vb: out.inf("starting video stream")
+    v.video_mode_on()
+    if vb: out.suc("completed video")
+
 def vrec(file, vb=False):
-    if ckcon():
-        if vb: out.inf("starting video recording")
-        v.start_recording(file)
-        if vb: out.suc("stored video recording")
- 
+    if vb: out.inf("starting video recording")
+    v.start_recording(file)
+    if vb: out.suc("stored video recording")
+
 def vstop(vb=False):
-    if ckcon():
-        if vb: out.inf("stopping video recording")
-        v.stop_recording()
-        if vb: out.suc("stopped video recording")
- 
+    if vb: out.inf("stopping video recording")
+    v.stop_recording()
+    if vb: out.suc("stopped video recording")
+
 def vsize(vb=False):
-    if ckcon():
-        if vb: out.inf("getting video resolution")
-        vr = v.get_image_size()
-        if vb: out.suc("completed video resolution retrieval")
-        return vr
- 
-def vsr(vb=False):
-    if ckcon():
-        if vb: out.inf("stoping live display")
-        v.stop_live()
-        if vb: out.suc("stopped live display")
- 
+    if vb: out.inf("getting video resolution")
+    vr = v.get_image_size()
+    if vb: out.suc("completed video resolution retrieval")
+    return vr
+
+def vlive(vb=False):
+    if vb: out.inf("stoping live display")
+    v.stop_live()
+    if vb: out.suc("stopped live display")
+
 def vframe(vb=False):
-    if ckcon():
-        if vb: out.inf("getting video frame")
-        frame = v.get_video()
-        if vb: out.suc("completed getting video frame")
-        return frame
- 
-def vdec(frame, color):
+    if vb: out.inf("getting video frame")
+    frame = v.get_video()
+    if vb: out.suc("completed getting video frame")
+    return frame
+
+def vdet(frame, color):
     colors = {
         'blue': (np.array([100, 150, 70]), np.array([140, 255, 255])),
         'red': (np.array([0, 90, 100]), np.array([10, 255, 255])),
@@ -277,13 +248,15 @@ def vdec(frame, color):
         return center_x, center_y, frame
     else:
         return None, None, frame
- 
+
 def tfinit(model, label):
-    if ckcon():
-        global td
-        td = det.tflite_detector(model=model, label=label)
- 
+    global td
+    if model[:5] == "path/":
+        model = r"C:/Users/34/Desktop/drone/model/" + model[5:]
+    if label[:5] == "path/":
+        label = r"C:/Users/34/Desktop/drone/model/" + label[5:]
+    td = det.tflite_detector(model=model, label=label)
+
 def tfdet(frame):
-    if ckcon():
-        nframe = td.detect(frame)
-        return nframe
+    nframe = td.detect(frame)
+    return nframe
