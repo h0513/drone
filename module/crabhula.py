8cded8ccb1ebb2693a6630c9516d43f01abca9eb
shawn-foo <foojw.shawn@gmail.com>
2025-04-05 14:11:41 +0800

    deleted pycache folders in three folders

--- module/crabhula.py
+++ module/crabhula.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 from types import UnionType
-from typing import Optional, TypedDict, Literal, TypeAlias
+from typing import Optional, TypedDict, Literal, TypeAlias, Dict
 import pyhula as hula
 
 from typing import TypedDict, Literal, TypeAlias, Union
@@ -18,6 +18,7 @@ class userAPI:
     A class representing a programmable drone with various capabilities like flying,
     camera usage, LED control, and interaction with tags and QR codes.
     """
+    api: hula.userAPI
 
     def __init__(self):
         self.api = hula.UserApi()
@@ -26,109 +27,119 @@ class userAPI:
         '''
         connects to the drone
         '''
-        self.api.connect(server_ip)
+        return self.api.connect(server_ip)
     def single_fly_takeoff(self, led: LedT = 0):
         '''
         drone takes off
         '''
-        self.api.single_fly_takeoff(led)
+        return self.api.single_fly_takeoff(led)
     def single_fly_touchdown(self, led: LedT = 0):
         '''
         drone lands
         '''
-        self.api.single_fly_touchdown(led)
+        return self.api.single_fly_touchdown(led)
     def single_fly_left(self, distance: int, led: LedT = 0):
         '''
         flies in left direction
         '''
-        self.api.single_fly_left(distance, led)
+        return self.api.single_fly_left(distance, led)
     def single_fly_forward(self, distance: int, led: LedT = 0):
         '''
         flies in forward direction
         '''
-        self.api.single_fly_forward(distance, led)
+        return self.api.single_fly_forward(distance, led)
     def single_fly_right(self, distance: int, led: LedT = 0):
         '''
         flies in right direction
         '''
-        self.api.single_fly_right(distance, led)
+        return self.api.single_fly_right(distance, led)
     def single_fly_back(self, distance: int, led: LedT = 0):
         '''
         flies in back direction
         '''
-        self.api.single_fly_back(distance, led)
+        return self.api.single_fly_back(distance, led)
     def single_fly_up(self, height: int, led: LedT = 0):
         '''
         flies in up direction
         '''
-        self.api.single_fly_up(height, led)
+        return self.api.single_fly_up(height, led)
     def single_fly_down(self, height: int, led: LedT = 0):
         '''
         flies in down direction
         '''
-        self.api.single_fly_down(height, led)
+        return self.api.single_fly_down(height, led)
     def single_fly__turnleft(self, angle: int, led: LedT = 0):
         '''
         turns left
         '''
-        self.api.single_fly_turnleft(angle, led)
+        return self.api.single_fly_turnleft(angle, led)
     def single_fly__turnright(self, angle: int, led: LedT = 0):
         '''
         turns right
         '''
-        self.api.single_fly_turnright(angle, led)
+        return self.api.single_fly_turnright(angle, led)
     def single_fly__bounce(self, bounces: int, height: int, led: LedT = 0):
         '''
         bounces, quite anticlimatic
         '''
-        self.api.single_fly_turnright(bounces, height, led)
+        return self.api.single_fly_turnright(bounces, height, led)
     def single_fly_straight_flight(self, x: int, y: int, z: int, led: LedT = 0):
         '''
         flies to that cartision coordinate
         '''
-        self.api.single_fly_straight_flight(x, y, z, led)
+        return self.api.single_fly_straight_flight(x, y, z, led)
     def single_fly_radius_around(self, radius: int, led: LedT = 0):
         '''
         flies around a set point
         '''
-        self.api.single_fly_radius_around(radius, led)
+        return self.api.single_fly_radius_around(radius, led)
     def single_fly_autogyration360(self, num, led: LedT = 0):
         '''
         rotates the drone 360 degress counterclockwise for num times
         '''
-        self.api.single_fly_autogyration360(num, led)
+        return self.api.single_fly_autogyration360(num, led)
     def single_fly_somersault(self, direction: Literal[0, 1, 2, 3], led: LedT = 0):
         '''
         flips
         '''
-        self.api.single_fly_somorsault(direction, led)
+        return self.api.single_fly_somorsault(direction, led)
     def single_fly_curvilinearFlight(self, x:int, y: int, z: int, direction: bool = True, led: LedT = 0):
         '''
         what the fish is this
         '''
-        self.api.single_fly_curvilinearFlight(direction, x, y, z, led)
+        return self.api.single_fly_curvilinearFlight(direction, x, y, z, led)
     def single_fly_hover_flight(self, time: int, led: LedT = 0):
         '''
         hover at a point for a time; use instead of time.sleep
         '''
-        self.api.single_fly_hover_flight(time, led)
+        return self.api.single_fly_hover_flight(time, led)
     def single_fly_barrier_aircraft(self, mode: bool):
         '''
         enable/disable obstacle avoidance
         '''
-        self.api.single_fly_barrier_aircraft(mode)
+        return self.api.single_fly_barrier_aircraft(mode)
     def line_walking(self, fun_id: Literal[1], dist: int, way_color: int):
         '''
         fun id 0 following lines, ignoring detection
         '''
-        self.api.single_fly_Line_walking(fun_id, dist, way_color)
+        return self.api.single_fly_Line_walking(fun_id, dist, way_color)
     def AiIdentifies(self, mode: int):
         '''
         recognises various signs
         TODO
         '''
-        self.api.single_flyAiIdentifies(mode)
+        return self.api.single_flyAiIdentifies(mode)
     def Qrcode_align(self, mode: int, qr_id: int):
+        '''
+        alings to the qr code
+        TODO test
+        '''
+        return self.api.single_fly_Qrcode_align(mode, qr_id)
+    def Qrcode_recognition(self, mode: int, qr_id: int) -> Dict[str, Union[str, bool]]:
+        '''
+        returns data about a qr code in frame
+        '''
+        return self.api.single_fly_recognition_Qrcode(mode, qr_id)
 
 
 
