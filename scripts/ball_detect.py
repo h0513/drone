d7e93c7f891e18aff38d18e71289d6632d5a8787
h0513 <handle@null.net>
2025-04-05 20:31:36 +0800

    Update ball_detect.py

--- scripts/ball_detect.py
+++ scripts/ball_detect.py
@@ -1,6 +1,5 @@
 import module
 import drone as d
-import output as out
 import cv2 as cv
 import numpy as np
 import time
@@ -8,12 +7,12 @@ import time
 d.start()
 d.vinit()
 d.von()
-d.vrec("photo")
+d.vrec("image")
  
 time.sleep(2)
 for i in range(100):
     frame = d.vframe()
-    bframe = d.vdet(frame)
+    bframe = d.vdet(frame, "blue")
     cv.imshow("Ball Detection", bframe[2])
     cv.waitKey(1)
     time.sleep(0.5)
