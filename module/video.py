df69a65ae8beabd30ea443f51dbeb250b0b0ef68
h0513 <handle@null.net>
2025-04-05 20:33:15 +0800

    Update video.py

--- module/video.py
+++ module/video.py
@@ -10,9 +10,16 @@ from collections import deque
 import os
 import output as out
 
-class vid:
-    def __init__(self,api,hula_ip="0.0.0.0",display=False):
-        self.uapi = api
+class hula_video:
+    def __init__(self,hula_api,hula_ip="0.0.0.0",display=False):
+        '''
+        Initialize the hula_video with the pyhula object.
+        display parameter: Tell the hula_video to display video stream or not.
+        Example:
+            video = hula_video(hula_api=uapi,display=False)
+
+        '''
+        self.uapi = hula_api
         self.hula_ip = hula_ip
         self.video_port = 9000 + (self.uapi.get_plane_id() * 2)
         self.live = display
@@ -41,7 +48,7 @@ class vid:
         self.lib.has_updated_frame.restype = None
         self.stopApp = False
         self.record = False
-        self.photo_filename = "photo"
+        self.photo_filename = "image_"
         self.photo_index= 0
         self.detect = False
         self.confidence_level = 0.5
@@ -49,48 +56,72 @@ class vid:
         self.detected_object_score = []
         self.detecting = False
         self.buffer_size = 25
-        self.savepath = os.path.join(os.getcwd(),'photo')
+        self.savepath = os.path.join(os.getcwd(),'image')
         if not os.path.exists(self.savepath):
-            out.inf("creating save directory")
+            out.inf("creating save directory image")
             os.makedirs(self.savepath)
 
     def __del__(self):
         self.close()
 
     def close(self):
+        '''
+        Close the video stream.
+        '''
         self.stopApp = True
 
-    def readframe(self):
+    def read_frame(self):
         if self.video_queue.empty == False:
             return self.video_queue.pop()
 
-    def vo(self):
+    def video_mode_on(self):
+        '''
+        Call this function to turn on the video stream from the drone. This must be called after initializing a hula_video object.
+        '''
         self.stopApp = False
         self.uapi.Plane_cmd_swith_rtp(0)
-        out.inf("starting video stream")
+        out.inf('starting video stream')
         time.sleep(1)
         ret = self.lib.init_video(self.hula_ip,self.video_port)
         while self.lib.get_rgb_datas_length() <= 0:
             pass
-        out.suc('completed video stream startup')
+        out.suc('video stream started')
         self.videothread.start()
 
-    def sr(self,filename="photo"):
+    def start_recording(self,filename="image"):
+        '''
+        Call this function to tell hula_video to store the video received from the drone into individual files.
+        parameter:
+        filename - defines what the photo files are prepended.
+        '''
         self.record = True
         self.photo_filename = filename
         self.photo_index = 0
 
-    def srec(self):
+    def stop_recording(self):
+        '''
+        Call this function to tell hula_video to stop recording.
+        '''
         self.record = False
 
-    def vrs(self):
+    def get_image_size(self):
+        '''
+        Call this function to find out what is the resolution of the video.
+        Returns a tuple of height and width of video resolution.
+        '''
         return (self.lib.get_height(),self.lib.get_width())
 
-    def sl(self):
+    def stop_live(self):
+        '''
+        Call this function to stop the live display.
+        '''
         self.live = False
         self.video_queue = deque()
 
-    def gv(self,get_latest=True,keep_getting=True):
+    def get_video(self,get_latest=True,keep_getting=True):
+        '''
+        Call this function to get a frame of the video. It returns the latest frame in the form of numpy array.
+        '''
         while len(self.video_queue) == 0 and keep_getting and self.stopApp == False:
             time.sleep(0.1)
         frame = None
@@ -103,7 +134,7 @@ class vid:
 
 
     def _receive_video_data(self):
-        out.inf(f"started video receiver for drone {self.video_port}")
+        out.suc(f'started video receiver for drone {self.video_port}')
         length = self.lib.get_rgb_datas_length()
         self.vid_width = self.lib.get_width()
         self.vid_height = self.lib.get_height()
@@ -134,4 +165,4 @@ class vid:
         if self.display:
             cv2.destroyWindow('Video')
         self.uapi.Plane_cmd_swith_rtp(1)
-        out.inf("closed video stream")
+        out.suc('video stream closed')
